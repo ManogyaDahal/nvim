5190c99dd0d27168c4e9ad907943a1a2dfa8dba2
Manogya Dahal <bat16692@gmail.com>
2026-08-26 15:36:21 +0545

    colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,7 @@
+require('vague').setup({
+  bold = true,
+  italic = true,
+})
 vim.cmd("colorscheme vague") -- or whatever theme you use
-
 -- Clear main background
 vim.cmd( "highlight Normal ctermbg=None guibg=None" )
